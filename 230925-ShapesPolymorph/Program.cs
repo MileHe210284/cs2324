6d34ba930faff8054174cc792ff51dabcfbd87b4
Helena Milekic <40146720210284@litec.ac.at>
2023-09-25 09:52:26 +0200

    New Project added

---
+++ 230925-ShapesPolymorph/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _230925_ShapesPolymorph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
